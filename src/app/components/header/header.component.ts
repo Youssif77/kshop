38f82d48bc42238fed6ec49b784995a231441389
youssif hany <youssifhany75@gmail.com>
2022-02-26 11:14:41 +0200

    finish header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NzButtonSize } from 'ng-zorro-antd/button';
+import { NzCarouselComponent } from 'ng-zorro-antd/carousel';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  carouselImgs = ['hero-img-1.png', 'hero-img-2.jpg', 'hero-img-3.png'];
 
-  constructor() { }
+  visible = false;
 
-  ngOnInit(): void {
+  open(): void {
+    this.visible = true;
   }
 
+  close(): void {
+    this.visible = false;
+  }
+
+  @ViewChild('carousel') carousel!: NzCarouselComponent;
+
+  size: NzButtonSize = 'large';
 }
--- src/app/zorro.module.ts
+++ src/app/zorro.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
+
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
 import { NzRateModule } from 'ng-zorro-antd/rate';
 import { NzCarouselModule } from 'ng-zorro-antd/carousel';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 
 @NgModule({
-  exports: [NzRateModule, NzCarouselModule],
+  exports: [
+    NzIconModule,
+    NzButtonModule,
+    NzInputModule,
+    NzLayoutModule,
+    NzDrawerModule,
+    NzRateModule,
+    NzCarouselModule,
+    NzDropDownModule,
+  ],
 })
 export class ZorroModule {}
